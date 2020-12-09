65f8f2ad3ab679cbc42ca413ec3f38da695f5998
Samrood Ali <samroodali@live.com>
2020-12-09 08:40:37 +0000

    game logics

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from art import logo
 import random
 from replit import clear
+clear()
 print(logo)
 
 #################################################### NUMBER GUESSING GAME ################################################################
@@ -8,9 +9,52 @@ print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I am thinking of a number between 1 and 100")
 number_chosen = random.randint(1,100)
-print("Psst,the correct number is :",number_chosen)
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': hard")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard':\n")
 if difficulty =='hard':
     lives = 5
 else:
     lives = 10
+
+print(f"You have {lives} attempts remaining to guess the number.")
+
+while not lives == 0:
+    guess = int(input("Make a guess : "))
+    if guess==number_chosen:
+        print("You guessed right")
+        lives == 0
+        break
+    difference = number_chosen - guess
+    if difference > 50:
+        prompt ='very very low'
+        lives -= 1
+        print(f"You have {lives} attempts remaining to guess the number.")
+    elif difference > 25:
+        prompt ='too low'
+        lives -= 1
+        print(f"You have {lives} attempts remaining to guess the number.")
+    elif difference > 10:
+        prompt='low'
+        lives -= 1
+        print(f"You have {lives} attempts remaining to guess the number.")
+    else:
+        prompt='near'
+        lives -= 1
+        print(f"You have {lives} attempts remaining to guess the number.")
+    if difference < 0:
+        if difference > -50:
+            prompt ='very very high'
+            lives -= 1
+            print(f"You have {lives} attempts remaining to guess the number.")
+        elif difference > -25:
+            prompt ='too high'
+            lives -= 1
+            print(f"You have {lives} attempts remaining to guess the number.")
+        elif difference > -10:
+            prompt='high'
+            lives -= 1
+            print(f"You have {lives} attempts remaining to guess the number.")
+        else:
+            prompt='near'
+            lives -= 1
+            print(f"You have {lives} attempts remaining to guess the number.")
+    print(prompt)
