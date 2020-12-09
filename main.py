d849fc9141f460e8fe2d0dd799fcd8b0ad28b6d3
Samrood Ali <samroodali@live.com>
2020-12-09 15:23:51 +0000

    Reworked the entire project, split everything functionality

--- main.py
+++ main.py
@@ -7,54 +7,58 @@ print(logo)
 #################################################### NUMBER GUESSING GAME ################################################################
 
 print("Welcome to the Number Guessing Game!")
-print("I am thinking of a number between 1 and 100")
-number_chosen = random.randint(1,100)
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard':\n")
-if difficulty =='hard':
-    lives = 5
-else:
-    lives = 10
 
-print(f"You have {lives} attempts remaining to guess the number.")
+def number_generator(num):
+    """Generates a random number between 1 and 100"""
+    print("I am thinking of a number between 1 and 100")
+    number_chosen = random.randint(1,num)
+    print("Psst,the correct number is :",number_chosen)
+    return number_chosen
 
-while not lives == 0:
-    guess = int(input("Make a guess : "))
-    if guess==number_chosen:
-        print("You guessed right")
-        lives == 0
-        break
-    difference = number_chosen - guess
-    if difference > 50:
-        prompt ='very very low'
-        lives -= 1
-        print(f"You have {lives} attempts remaining to guess the number.")
-    elif difference > 25:
-        prompt ='too low'
-        lives -= 1
-        print(f"You have {lives} attempts remaining to guess the number.")
-    elif difference > 10:
-        prompt='low'
-        lives -= 1
-        print(f"You have {lives} attempts remaining to guess the number.")
+
+def lives_genertor(difficulty):
+    """Generates lives based on difficulty"""
+    if difficulty =='hard':
+        lives = 5
     else:
-        prompt='near'
+        lives = 10
+    return lives
+
+
+def guess_listener():
+    guess = int(input("Make a guess: "))
+    return guess
+
+def guess_verification(guess,chosen_number,lives):
+    if guess==chosen_number:
+        print("You won")
+        return 0
+    elif lives > 1:
         lives -= 1
-        print(f"You have {lives} attempts remaining to guess the number.")
-    if difference < 0:
-        if difference > -50:
-            prompt ='very very high'
-            lives -= 1
-            print(f"You have {lives} attempts remaining to guess the number.")
-        elif difference > -25:
-            prompt ='too high'
-            lives -= 1
-            print(f"You have {lives} attempts remaining to guess the number.")
-        elif difference > -10:
-            prompt='high'
-            lives -= 1
-            print(f"You have {lives} attempts remaining to guess the number.")
+        if guess > chosen_number:
+            print("too high")
         else:
-            prompt='near'
-            lives -= 1
-            print(f"You have {lives} attempts remaining to guess the number.")
-    print(prompt)
+            print("too low")
+    else:
+        print(f"Sorry,the number was {chosen_number}. You lose")
+        return 0
+    return lives
+        
+
+def game_start(num):
+    """Main Game Function taking the max number"""
+    # Generate a random number between 1 and 100
+    number = number_generator(100)
+
+    # get difficulty
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard':\n")
+
+    # Initialising lives(no of attempts)
+    lives = lives_genertor(difficulty)
+    
+    while not lives == 0:
+        print(f"You have {lives} attempts remaining to guess the number.")
+        guess = guess_listener()
+        lives = guess_verification(guess,number,lives)
+        
+game_start(100)
