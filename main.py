f633b348a7a9eb08519011e08c81a48ad42f7cbd
Samrood Ali <samroodali@live.com>
2020-12-09 08:06:13 +0000

    imports

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from art import logo
+import random
+from replit import clear
 print(logo)
+
+#################################################### NUMBER GUESSING GAME ################################################################
+
