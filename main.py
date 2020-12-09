c7c19cb10840a69b517eed2ba102de796b43a9eb
Samrood Ali <samroodali@live.com>
2020-12-09 08:13:36 +0000

    Add:lives

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ print("I am thinking of a number between 1 and 100")
 number_chosen = random.randint(1,100)
 print("Psst,the correct number is :",number_chosen)
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': hard")
+if difficulty =='hard':
+    lives = 5
+else:
+    lives = 10
