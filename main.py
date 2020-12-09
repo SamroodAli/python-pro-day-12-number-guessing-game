c0ec187791ff159321509b6e05f114c142cb0c0f
Samrood Ali <samroodali@live.com>
2020-12-09 08:12:34 +0000

    Add:Initial prompts

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ print(logo)
 
 #################################################### NUMBER GUESSING GAME ################################################################
 
+print("Welcome to the Number Guessing Game!")
+print("I am thinking of a number between 1 and 100")
+number_chosen = random.randint(1,100)
+print("Psst,the correct number is :",number_chosen)
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': hard")
